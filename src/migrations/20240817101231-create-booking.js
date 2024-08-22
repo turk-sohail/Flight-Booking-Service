fef901048b83778921ad82538e554d078413b227
turk-sohail <ksohail470@gmail.com>
2024-08-22 10:36:04 +0500

    added booking model with utils folder

---
+++ src/migrations/20240817101231-create-booking.js
@@ -0,0 +1,51 @@
+const { Enums } = require("../utils");
+const { BOOKED, CANCELED, PENDING, INITIATED } = Enums.BOOKING_STATUS;
+
+("use strict");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Bookings", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      flightId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: [BOOKED, CANCELED, PENDING, INITIATED],
+        defaultValue: INITIATED,
+        allowNull: false,
+      },
+      noOfSeats: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      totalCost: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Bookings");
+  },
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,6 @@
 module.exports = {
-}
+	SuccessResponse: require("./common/success-response"),
+	ErrorResponse: require("./common/error-response"),
+	errorHandler: require("./errors/error-middleware"),
+	Enums: require("./common/enums"),
+};
